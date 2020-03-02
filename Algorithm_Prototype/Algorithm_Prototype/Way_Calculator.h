081e86309e017763ccee63e9983b89cfb727e218
SNecker33 <60650744+SNecker33@users.noreply.github.com>
2020-03-02 21:44:48 +0100

    ss

--- Algorithm_Prototype/Algorithm_Prototype/Way_Calculator.h
+++ Algorithm_Prototype/Algorithm_Prototype/Way_Calculator.h
@@ -1,27 +1,31 @@
-#pragma once
+#ifndef WAY_CALCULATOR_H
+#define	WAY_CALCULATOR_H
 
 #include<vector>
 #include <string>
-#include "Map.h"
-#include "Vertex.h"
+#include "City.h"
 using namespace::std;
 
 class Way_Calculator
 {
 protected:
-	int network[maxCitys][maxCitys];
+	int network[100][100];
 	Vertex start;
 	Vertex ende;
+<<<<<<< HEAD
 	vector<vector<City*>> waysFound;		// potential memory leak :(
+=======
+	vector<vector<City*>> waysFound;
+>>>>>>> f41afc77d19f88bc92aea49600dea939055e412d
 
 public:
 	// Default Constructor
 	Way_Calculator();
 
 	// Special Constructor
-	Way_Calculator(int network[maxCitys][maxCitys]);
+	Way_Calculator(int network[100][100]);
 
 	// Default Deconstructor
 	~Way_Calculator();
 };
-
+#endif
