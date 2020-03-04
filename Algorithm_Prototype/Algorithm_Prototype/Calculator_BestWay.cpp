aadf4fc5b6ee981b537214d00991d4863f325b03
00Wasp93 <33658641+00Wasp93@users.noreply.github.com>
2020-03-04 22:05:37 +0100

    s

--- Algorithm_Prototype/Algorithm_Prototype/Calculator_BestWay.cpp
+++
@@ -1,8 +0,0 @@
-#include "pch.h"
-#include "Calculator_BestWay.h"
-#include "Preprocessor_BestWay.h"
-
-
-void find_BestWay(int costAsReference) {
-	unsigned int i = Preprocessor_BestWay().get_costAsReference();
-}
